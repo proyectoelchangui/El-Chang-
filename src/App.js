45215eb75eae0a259ffca89947af28d7fc391fe0
gonzaloalonsopicco <gonzaloalonsopicco@gmail.com>
2023-03-07 17:10:19 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './componentes/Header/Header';
+import { Cuadroizquierda } from './componentes/Cuadroizquierda/Cuadroizquierda';
+import { Inicioagroecología } from './componentes/Inicioagroecología/Inicioagroecología';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Cuadroizquierda></Cuadroizquierda>
+          <Inicioagroecología></Inicioagroecología>
+
+          <Route path='/inicio' element={"hola"} />
+          <Route path="*" element={<p>error 404</p>} />
+        </Routes>
+      </BrowserRouter>
+
+
     </div>
   );
 }
