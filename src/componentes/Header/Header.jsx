2d8135f73dbaa4b927635b3bc441da42b5016ec7
blikroy <elroyvidal@gmai.com>
2023-03-07 17:41:52 -0300

    modificaciones

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -2,23 +2,24 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 
-export function Header() {
-    return (
+const Header = () => {
+    return(
         <div className='divHeader'>
-            <div className='container'>
-                <div className='divLogo'>
-                    <p className=''>el changui</p>
-                </div>
-                <nav className='divNavbar'>
-                    <ul>
-                        <li><Link to="/">Quiénes somos</Link></li>
-                        <li><Link to="/">Qué hacemos</Link></li>
-                        <li><Link to="/">Ecogranja</Link></li>
-                        <li><Link to="/">Voluntariado</Link></li>
-                        <li><Link to="/">Contactános</Link></li>
-                    </ul>
-                </nav>
+        <div className='container'>
+            <div className='divLogo'>
+                <p className=''>el changui</p>
             </div>
+            <nav className='divNavbar'>
+                <ul>
+                    <li><Link to="/">Quiénes somos</Link></li>
+                    <li><Link to="/">Qué hacemos</Link></li>
+                    <li><Link to="/">Ecogranja</Link></li>
+                    <li><Link to="/">Voluntariado</Link></li>
+                    <li><Link to="/">Contactános</Link></li>
+                </ul>
+            </nav>
         </div>
-    )
+    </div>
+    );
 }
+export default Header
